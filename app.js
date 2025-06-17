503320b33cd552ed25c7e33012e68053aabfefa2
sharva-save <sharvasave2509@gmail.com>
2025-06-17 16:42:31 +0530

    changes in the app.js to have the basices of the connected and disconnected functinallity

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const http = require("http");
+const path = require("path");
+const { Chess } = require("chess.js");
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const server = http.createServer(app);
+const io = socket(server);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const chess = new Chess();
+
+let player = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection" , function(uniquesocket){
+  console.log("connected");
+
+  // uniquesocket.on("send",function(){
+  //   io.emit("churan papadddd")
+  //   // console.log("send recevied");
+  // })
+  // uniquesocket.on("disconnect" , function(){
+  //   console.log("disconnectedddd");
+    
+  // })
+
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ public/javascript/chessgame.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+socket.emit("send")
+// socket.on("churan papadddd" , function(){
+//                console.log("churan papdi recevieddd");
+               
+// })
